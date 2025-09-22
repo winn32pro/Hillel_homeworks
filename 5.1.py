3e8ad6c81f219ff606df8c80357c31282678992e
Oleksandr <winn32pro@gmail.com>
2025-09-22 13:15:08 +0300

    5.1 homework

--- 5.1.py
+++ 5.1.py
@@ -1,22 +1,12 @@
-import sys
+import keyword
+import string
 
 usr_input = str(input("Enter a string: "))
-print(usr_input)
-key_list = ['False', 'None', 'True', 'and', 'or', 'not', 'as', 'assert'
-            'break', 'continue', 'def', 'del', 'elif', 'else', 'class',
-            'except', 'finally', 'finally', 'for', 'from', 'global',
-            'exec', 'if', 'import', 'in', 'is', 'lambda','nonlocal',
-            'pass', 'print', 'raise', 'return', 'try', 'while', 'with',
-            'yield']
-punctuation_list = ['(', ')', '[', ']', '{', '}' ',', ':', '.', ';', '@',
-                    '=', '->', '+=', '-=', '*=', '/=', '//=', '%=', '@=',
-                    '&=', '|=', '^=', '>>=', '<<=', '**=', ' ', '/', '!']
-for i in punctuation_list:
-    if usr_input.find(i) != -1:
-       print('False')
-    break
+print(usr_input.count("_"))
+
+if usr_input in keyword.kwlist or any(i.isupper() for i in usr_input) or any(i in string.punctuation.replace("_", "") for i in usr_input) or usr_input[0].isdigit():
+    print('False')
+elif any(i.isspace() for i in usr_input) or '__' in usr_input:
+    print('False')
 else:
-    if usr_input in key_list or usr_input.islower() is False or usr_input.isdigit() is True:
-        print('False')
-    else:
-        print('True')
+    print('True')
