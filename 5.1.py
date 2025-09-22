9f5eb51cf61a1cd0914d859abb3068996a02e4c4
Oleksandr <winn32pro@gmail.com>
2025-09-22 13:16:42 +0300

    5.1 homework

--- 5.1.py
+++ 5.1.py
@@ -2,7 +2,7 @@ import keyword
 import string
 
 usr_input = str(input("Enter a string: "))
-print(usr_input.count("_"))
+#print(usr_input.count("_"))
 
 if usr_input in keyword.kwlist or any(i.isupper() for i in usr_input) or any(i in string.punctuation.replace("_", "") for i in usr_input) or usr_input[0].isdigit():
     print('False')
