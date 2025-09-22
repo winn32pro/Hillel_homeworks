e7cf1799031ed8981ce19091883933a6b5b96182
Oleksandr <winn32pro@gmail.com>
2025-09-22 15:26:04 +0300

    5.1 update

--- 5.1.py
+++ 5.1.py
@@ -4,9 +4,9 @@ import string
 usr_input = str(input("Enter a string: "))
 #print(usr_input.count("_"))
 
-if usr_input in keyword.kwlist or any(i.isupper() for i in usr_input) or any(i in string.punctuation.replace("_", "") for i in usr_input) or usr_input[0].isdigit():
+if usr_input in keyword.kwlist or any(i.isupper() for i in usr_input) or any(i in string.punctuation.replace("_", "") for i in usr_input) or not usr_input:
     print('False')
-elif any(i.isspace() for i in usr_input) or '__' in usr_input:
+elif any(i.isspace() for i in usr_input) or usr_input[0].isdigit() or usr_input[:2].count("_") > 1:
     print('False')
 else:
     print('True')
